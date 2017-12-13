c346a2c368c34631496af21bb66d75a36992d74f
ubuntu <ubuntu@h1016.nci.nih.gov>
2017-12-13 16:07:24 -0500

    fixed disk test

--- linktest.py
+++ linktest.py
@@ -1,8 +1,8 @@
 import timeit, os
 
-mrcs_loc = ""
-link_loc = ""
-num_mrcs = 200
+mrcs_loc = "/home/ubuntu/local_test/Micrographs"
+link_loc = "/home/ubuntu/local_test/Links"
+num_mrcs = 100
 
 
 # TODO: Somehow eliminate the effects of disk caching
@@ -11,16 +11,18 @@ def linktest(use_syscall):
     for i in range(num_mrcs):
         mrc_name = "{}/{}.mrc".format(mrcs_loc, i)
         link_name = "{}/{}.mrc".format(link_loc, i)
-        links[i] = link_name
-        if use_syscall:
+        links.append(link_name)
+        if not use_syscall:
             os.symlink(mrc_name, link_name)
         else:
-            os.system("ln -s {} {}".format(link_name, link_name))
+            os.system("ln -s {} {}".format(mrc_name, link_name))
 
     # Clean up
     [os.unlink(link) for link in links]
 
 
 if __name__ == "__main__":
-    print(timeit.timeit("linktest(False)", setup="from __main__ import linktest()"))
-    print(timeit.timeit("linktest(True)", setup="from __main__ import linktest()"))
+    print("Running link test (os.symlink())")
+    print("os.symlink(): {}\n".format(timeit.timeit("linktest(False)", setup="from __main__ import linktest", number=1)))
+    print("Running link test (system call)")
+    print("os.system(): {}".format(timeit.timeit("linktest(True)", setup="from __main__ import linktest", number=1)))
